27a2aa3923954929549f19c2d9553f92ad27aaa7
Robert Ingrum <me@robertingrum.com>
2022-08-26 22:42:28 -0600

    https://github.com/robertIngrum/UnrealSandbox.gi://github.com/robertIngrum/UnrealSandbox.git

---
+++ README.md
@@ -0,0 +1,3 @@
+# UnrealSandbox
+
+Developed with Unreal Engine 5
---
+++ Source/UnrealSandbox/UnrealSandboxGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealSandboxGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealSandboxGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealSandboxGameMode();
+};
+
+
+
